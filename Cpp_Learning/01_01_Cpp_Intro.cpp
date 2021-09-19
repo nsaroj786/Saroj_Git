ee8ffd27b829e3a4555273d3b9997a0c60ad0354
Saroj Kumar N <nsaroj.786@gmail.com>
2021-09-19 17:54:13 +0530

    C++ basics learning files

---
+++ Cpp_Learning/01_01_Cpp_Intro.cpp
@@ -0,0 +1,59 @@
+/*Introduction to c++ programming
+	Execution process of c++ code
+	Basic structure of a c++ programming
+
+	author		: Saroj Kumar N
+	craeted on	: 16/09/2021 
+	modified on	: 17/09/2021
+*/
+//C with classes -> c++, c++11, c++14, c++17, .cpp & .h extension
+/* Execution process of c/c++
+	source code ->	high-level, what programmer write, undestandable by human, text-editor is used to write the code
+					many .cpp, one main.cpp which calls all executions
+	pre-processor->	removes comments, includes required headers, replaces macro-name by code
+	compiler ->		translates from hogh-level to low-level, 
+					will create .obj file for each .cpp
+	linker ->		links together our code with other libraries or headers, creates executable code
+					will include c++ and other libraries to create an executable program. in windows .exe and in linux no extension
+*/
+// IDE -> Integrated Development Environment, provides editor, cjompiler, linker, debugger together. eg- eclipse, visual studio
+//Errors -> compiler: syntax and semantic, compiler warning, linker error, runtime error, logic error
+
+/*Structure of a c++ program 
+	Basic components	:
+		keywords		: total 90,  which are reserved words for the programming language. eg: return, int, char
+		identifiers		: alphanumeric charachetrs which are defined by programmer (max 31 latters, start with alphabat or _), eg: cin, cout, main
+		punchuation		: ; " " { }
+		statement		: is a comination of keyword, identifier and punchuation
+	Preprocessor directives:
+		runs before the compiler executes, is a compiler ddirective not a statement so not required ; at the end
+		# symblolic constant is defined with uppercase, illegal to change the value throughtout the program 
+		# include , #ifndef
+	The main function:
+		every progrram must have one main program  return_type main (arguments)
+		return 0 indicates successful program execution
+	Namespaces:
+		is used to avoid naming conflicts
+		std::cout because std is the name for C++ standard namespace
+		or define for entire code -> using namespace std; or few from C++ std as -> using std::cin; using std::cout;
+	Comments: 
+		refernce for the programmer, not taken for compilation. single line ->//, multi-line -> /*
+	Basic I/O:
+		cin, cout, cerr, clog are objects representing streams, need to include iostream to use
+	*/
+
+#include<iostream>		// for basic I/O streams
+
+using namespace std;	// for using default c++ standards
+
+/*int main()
+{
+	int num;
+	cout << "Hello world !!" << endl;
+	cout << "Enter an integer. " << endl;
+	cin >> num;
+	cout << "Entered number is: " << num << endl;
+	
+
+	return 0;
+} */
---
+++ Cpp_Learning/01_02_Constants_Variables.cpp
@@ -0,0 +1,75 @@
+/*Introduction to c++ programming
+	Variables, COnstants, datatypes in c++
+
+	author		: Saroj Kumar N
+	craeted on	: 18/09/2021
+	modified on	: 19/09/2021
+*/
+
+/*
+Declare and Initialize variables:
+	declaration syntax: data_type variable_name;	eg: int age; string name;
+	can use alphanumeric or _, excluding the keywords, can not duplicate name in the same scope
+	Never use variables before initialization
+	
+	int age;		//un-initialization
+	Initialization:
+		int age =18; //c-like initilization
+		int age (18);//constructor initilization
+		int age {18};//c++11 list initilization
+
+	Global and local variable:
+		variable declared inside a function is local in that function
+		variable declared outside a function is global
+		if the global and local variable name is exactly same, its preferred to use local variable value
+*/
+/*
+Constant: constant values, compiler does not allow to change tha value
+	literal	: x =12 or 12U for unsigned int, 12L for long int, 12LL for long long int, float 12F for float, float 12L for double
+			  character literal constants : followed by \ chara, eg - \n - new line, \r - return, \t- tab, \b - backspace 
+	const	: const double pi = 3.141;
+	constexpr:
+	enum	:
+	#define	: #define pi 3.141, dont use #define constants in C++ 14
+*/
+
+
+/*
+datatypes: primitive or primary or fundamental and secondary datatypes
+Primitive or Primary datatypes:
+	int: signed and unsigned
+			int: 16bits, short int: 8bits, long int: 32 bits, long long int: 64 bits
+			bydefault integer is a signed integer -x tp 0 to x. for unsigned integer have to declared
+	float: used to represent non-int values, eg: pie, value: 3.17
+			float: 7 ecimal digits, double: 15, long double:19
+	char:	used to represent single characters like 'a' 'x'
+			char: 8bits, char16_t: 16bits, char32_t: 32bits 
+	boolean: true or false
+			size: 8 bits
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int age = 10;
+
+//Program to calculate area of a room
+int main()
+{
+	int age;		//declaration, without initialization its value will be some garbage.
+	age = 18;
+	cout << "Age is : " << age << endl;		// age =18 , as local variable value is preferred first
+
+	int room_width, room_length, room_area;
+	cout << "Enter room width : " << endl;
+	cin >> room_width;
+	cout << "ENter room length : " << endl;
+	cin >> room_length;
+
+	room_area = room_width * room_length;
+	cout << "The room area is : "<< room_area << "sq feet" <<endl;
+	return 0;
+}
+
